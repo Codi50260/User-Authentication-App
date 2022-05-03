028f87d1f1c7af6240f204a4c3c86526961e7509
Codi50260 <codi@trixta.com>
2022-05-03 22:26:19 +0200

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $email = $_POST['email'];
-    $password = $_POST['pass'];
-
-    echo "You have successfully logged in as:" . "<br>";
-    echo $email . "<br>";
-    echo $password;
-?>
